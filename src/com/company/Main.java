856286cfddae1a7adec31fdce9648a6f7198f361
Ryan <ryan.d.miller@wsu.edu>
2018-05-24 12:38:19 -0700

    Created initial project, asks for input and reads file

---
+++ src/com/company/Main.java
@@ -0,0 +1,65 @@
+package com.company;
+// basic imports
+import java.io.*;
+import java.util.Scanner;
+// json imports
+//import org.json.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("*************\n*Hello World*\n*************\n");
+        String filename = getFilename();
+        String fileData = readFile(filename);
+        if(fileData != null) {
+            System.out.println(fileData);
+        }
+        else {
+            System.out.println("FAIL");
+        }
+        System.out.println("Thanks!");
+    }
+
+    public static String getFilename() {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("What file do you what to deduplicate: ");
+        String filename = reader.nextLine();
+        reader.close();
+        return filename;
+    }
+
+    public static String readFile(String filename) {
+        String results = "";
+
+        try {
+            FileReader fileR = new FileReader(filename);
+            BufferedReader buffR = new BufferedReader(fileR);
+            StringBuilder stringB = new StringBuilder();
+
+            String line = buffR.readLine();
+            while(line != null) {
+                stringB.append(line);
+                line = buffR.readLine();
+            }
+
+            results = stringB.toString();
+        }
+        catch(FileNotFoundException fileErr) {
+            System.out.println("Unable to access '" + filename + "'");
+            System.out.println("----------------Stacktrace----------------");
+            fileErr.printStackTrace();
+            System.out.println("------------------------------------------");
+            return null;
+        }
+        catch(IOException ioErr) {
+            System.out.println("Error reading file '" + filename + "'");
+            System.out.println("----------------Stacktrace----------------");
+            ioErr.printStackTrace();
+            System.out.println("------------------------------------------");
+            return null;
+        }
+
+        return results;
+    }
+}
